df11ff52635fa3cac7afa2f3c1041c713d1955b5
Raj Joshi <geeky2307@gmail.com>
2022-11-22 15:38:38 -0500

    modified decoder to prevent repeat digits

--- keypad_to_seven_segment/Decoder.v
+++ keypad_to_seven_segment/Decoder.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Company: Digilent Inc 2011
-// Engineer: Michelle Yu  
-//				 Josh Sackos
-// Create Date:    07/23/2012 
-//
-// Module Name:    Decoder
-// Project Name:   PmodKYPD_Demo
-// Target Devices: Nexys3
-// Tool versions:  Xilinx ISE 14.1 
-// Description: This file defines a component Decoder for the demo project PmodKYPD. The Decoder scans
-//					 each column by asserting a low to the pin corresponding to the column at 1KHz. After a
-//					 column is asserted low, each row pin is checked. When a row pin is detected to be low,
-//					 the key that was pressed could be determined.
-//
-// Revision History: 
-// 						Revision 0.01 - File Created (Michelle Yu)
-//							Revision 0.02 - Converted from VHDL to Verilog (Josh Sackos)
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// ==============================================================================================
-// 												Define Module
-// ==============================================================================================
 module Decoder(
     clk,
     Row,
@@ -29,18 +7,11 @@ module Decoder(
     DecodeOut
     );
 
-// ==============================================================================================
-// 											Port Declarations
-// ==============================================================================================
     input clk;						// 100MHz onboard clock
     input [3:0] Row;				// Rows on KYPD
     output [3:0] Col;			// Columns on KYPD
     output [3:0] DecodeOut;	// Output data
 
-// ==============================================================================================
-// 							  		Parameters, Regsiters, and Wires
-// ==============================================================================================
-	
 	// Output wires and registers
 	reg [3:0] Col;
 	reg [3:0] DecodeOut;
@@ -48,12 +19,8 @@ module Decoder(
 	// Count register
 	reg [19:0] sclk;
 
-// ==============================================================================================
-// 												Implementation
-// ==============================================================================================
-
 	always @(posedge clk) begin
-
+			DecodeOut <= 4'b1111;
 			// 1ms
 			if (sclk == 20'b00011000011010100000) begin
 				//C1
--- keypad_to_seven_segment/PmodKYPD.v
+++ keypad_to_seven_segment/PmodKYPD.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Company: Digilent Inc 2011
-// Engineer: Michelle Yu  
-//				 Josh Sackos
-// Create Date:    17:05:39 08/23/2011 
-//
-// Module Name:    PmodKYPD
-// Project Name:   PmodKYPD_Demo
-// Target Devices: Nexys3
-// Tool versions:  Xilinx ISE 14.1 
-// Description: This file defines a project that outputs the key pressed on the PmodKYPD to the 
-//					 seven segment display.
-//
-// Revision History: 
-// 						Revision 0.01 - File Created (Michelle Yu)
-//							Revision 0.01 - Converted from VHDL to Verilog (Josh Sackos)
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// ==============================================================================================
-// 												Define Module
-// ==============================================================================================
 module PmodKYPD(
     clk,
 	btnR,
@@ -30,23 +10,14 @@ module PmodKYPD(
 	led
     );
 	 
-	 
-// ==============================================================================================
-// 											Port Declarations
-// ==============================================================================================
 	input btnR;
-	input btnL;		// Button C
+	input btnL;
 	input clk;					// 100Mhz onboard clock
 	inout [7:0] JA;			// Port JA on Nexys3, JA[3:0] is Columns, JA[10:7] is rows
 	output [3:0] an;			// Anodes on seven segment display
 	output [6:0] seg;			// Cathodes on seven segment display
-	output [15:0] led;			// LEDs on Nexys3
-
-// ==============================================================================================
-// 							  		Parameters, Regsiters, and Wires
-// ==============================================================================================
+	output [15:0] led;
 	
-	// Output wires
 	wire [3:0] an;
 	wire [6:0] seg;
 	wire [3:0] hex_val;
@@ -56,13 +27,6 @@ module PmodKYPD(
     wire debounced_btnR;
     wire debounced_btnL;
 
-// ==============================================================================================
-// 												Implementation
-// ==============================================================================================
-
-	//-----------------------------------------------
-	//  						Decoder
-	//-----------------------------------------------
 	Decoder C0(
 			.clk(clk),
 			.Row(JA[7:4]),
@@ -82,9 +46,6 @@ module PmodKYPD(
         .pb_out(debounced_btnL)
     );
 
-	//-----------------------------------------------
-	//  		Seven Segment Display Controller
-	//-----------------------------------------------
 	DisplayController C1(
 			.btnR(debounced_btnR),
 			.btnL(debounced_btnL),
